3c05a9869d6d56882cef1f989818807be57a9365
Laprida <psmathe001@gmail.com>
2025-09-24 14:24:18 -0300

    controlador de article terminado, sin validaciones

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,27 +1,88 @@
 import { CommentModel } from "../models/comment.model.js";
 
-export const createComment = async (req,res) => {
+export const createComment = async (req, res) => {
+    const { content, author, article } = req.body;
+
     try {
-
-    } catch (error){
-
+        const newComment = await CommentModel.create({
+            content,
+            author,
+            article,
+        });
+        res.status(201).json({
+            ok: true,
+            msg: "Comment creado correctamente",
+            newComment,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json("Error interno del servidor", error);
     }
 };
 
-export const getAllComments = async (req,res) => {
+export const getAllComments = async (req, res) => {
     try {
-        
+        const comments = await CommentModel.find().populate("user");
+        res.status(200).json({
+            ok: true,
+            data: comments,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: "Error interno del servidor",
+        });
     }
 };
 
-export const getCommentById = async (req,res) => {
+export const getCommentById = async (req, res) => {
+    const { id } = req.params;
+    const { content, author, article } = req.body;
+    try {
+        const comment = await CommentModel.findById(id);
 
+        res.status(200).json({
+            ok: true,
+            data: comment,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: "Error interno del servidor",
+        });
+    }
 };
 
-export const updateComment = async (req,res) => {
+export const updateComment = async (req, res) => {
+    const { id } = req.params;
+    const { content, author, article } = req.body;
 
+    try {
+        const updatedComment = await CommentModel.findByIdAndUpdate(
+            id,
+            {
+                content,
+                author,
+                article,
+            },
+            { new: true }
+        );
+    } catch (error) {
+        console.log(error);
+        return.res(500).json({
+            ok: false,
+            msg: "Error interno del servidor",
+        });
+    }
 };
 
-export const deleteComment = async (req,res) => {
+export const deleteComment = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+    }
 
 };
